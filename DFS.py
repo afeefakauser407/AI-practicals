6807a4ed3fc222498e7302ad24a85b2fe75b11b5
Afeefa Kauser <155752128+afeefakauser407@users.noreply.github.com>
2024-05-06 12:21:31 +0530

    Create DFS.py

---
+++ DFS.py
@@ -0,0 +1,34 @@
+class Graph:
+    def _init_(self):
+        self.graph = {}
+
+    def add_edge(self, u, v):
+        if u not in self.graph:
+            self.graph[u] = []
+        if v not in self.graph:
+            self.graph[v] = []
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def dfs_util(self, v, visited):
+        visited.add(v)
+        print(v, end=' ')
+
+        for neighbor in self.graph[v]:
+            if neighbor not in visited:
+                self.dfs_util(neighbor, visited)
+
+    def dfs(self, start):
+        visited = set()
+        self.dfs_util(start, visited)
+
+# Example usage:
+g = Graph()
+g.add_edge(0, 1)
+g.add_edge(0, 2)
+g.add_edge(1, 2)
+g.add_edge(2, 3)
+g.add_edge(3, 3)
+
+print("Depth First Traversal starting from vertex 0:")
+g.dfs(0)
